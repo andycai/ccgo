65378bc879560bd82bed2e0853b2db77cde147f2
wayee <iwayee@gmail.com>
2013-01-23 18:43:27 +0800

    updated the app path

--- utils/lua.go
+++ utils/lua.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	TEMPLATE_PATH       string = "./kodelua/template/"
+	TEMPLATE_PATH       string = "./kode/template/"
 	TEMPLATE_CONTROLLER string = TEMPLATE_PATH + "controller.lua"
 	TEMPLATE_MODEL      string = TEMPLATE_PATH + "model.lua"
 	TEMPLATE_SERIVCE    string = TEMPLATE_PATH + "service.lua"
@@ -16,12 +16,12 @@ const (
 	TEMPLATE_REGISTER   string = TEMPLATE_PATH + "register.lua"
 	TEMPLATE_INIT       string = TEMPLATE_PATH + "initialize.lua"
 	EXPORT_PATH         string = "./protected/"
-	EXPORT_CONTROLLER   string = EXPORT_PATH + "ctrl/%s.lua"
+	EXPORT_CONTROLLER   string = EXPORT_PATH + "controller/%s.lua"
 	EXPORT_MODEL        string = EXPORT_PATH + "model/%s.lua"
-	EXPORT_SERVICE      string = EXPORT_PATH + "serv/%s.lua"
+	EXPORT_SERVICE      string = EXPORT_PATH + "service/%s.lua"
 	EXPORT_VIEW         string = EXPORT_PATH + "view/%s/"
 	EXPORT_REGISTER     string = EXPORT_PATH + "register.lua"
-	EXPORT_INIT         string = EXPORT_PATH + "init.lua"
+	EXPORT_INIT         string = EXPORT_PATH + "export.lua"
 )
 
 func LuaMakeModule(tokens []string) {
